1fc30fc99e56bec5875e4dc86f8d3d3fe06c5bad
Dor Zilberman <dorzil1998@gmail.com>
2024-03-09 19:41:37 +0200

    added comments

---
+++ models/comment/comment.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Post', postSchema);
